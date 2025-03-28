8e33ee958304cdcad35780a1870b5236a55d16cb
Ahmed Abas Al-Ali <bfhetyrhyr12@gmail.com>
2025-03-28 15:53:48 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
   useEffect(() => {
     const fetchNotes = async() => {
       try{
